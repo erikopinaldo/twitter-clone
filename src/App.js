4e9075108ccdc6e1890cbc86080aa636458d1296
Erik Opinaldo <erikopinaldo@gmail.com>
2023-07-09 16:59:16 -0400

    add AddPostForm component

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ function App() {
             path="/"
             render={() => (
               <React.Fragment>
+                <AddPostForm />
                 <PostsList />
               </React.Fragment>
             )}
