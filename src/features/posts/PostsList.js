e6ec03c692eb479f7e0114f4eb4a81c280e5e432
Erik Opinaldo <erikopinaldo@gmail.com>
2023-07-09 18:15:29 -0400

    add reactionButtons component

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -4,14 +4,17 @@ import { Link } from 'react-router-dom'
 
 import { PostAuthor } from './PostAuthor'
 import { TimeAgo } from './TimeAgo'
+import { ReactionButtons } from './ReactionButtons'
 
 export const PostsList = () => {
-    const posts = useSelector(state => state.posts)
+    const posts = useSelector((state) => state.posts)
 
     // Sort posts in reverse chronological order by datetime string
-    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+    const orderedPosts = posts
+        .slice()
+        .sort((a, b) => b.date.localeCompare(a.date))
 
-    const renderedPosts = orderedPosts.map(post => {
+    const renderedPosts = orderedPosts.map((post) => {
         return (
             <article className="post-excerpt" key={post.id}>
                 <h3>{post.title}</h3>
@@ -20,6 +23,8 @@ export const PostsList = () => {
                     <TimeAgo timestamp={post.date} />
                 </div>
                 <p className="post-content">{post.content.substring(0, 100)}</p>
+
+                <ReactionButtons post={post} />
                 <Link to={`/posts/${post.id}`} className="button muted-button">
                     View Post
                 </Link>
