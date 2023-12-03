6d352508e2952e7edd923a962044f551ae00cd9b
Erik Opinaldo <erikopinaldo@gmail.com>
2023-12-03 17:04:50 -0500

    initial view selector

---
+++ src/features/posts/ViewSelector.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const ViewSelector = () => {
+    return (
+        <section className='view-selector-wrapper'>
+            <ul className='view-selector'>
+                <li>For You</li>
+                <li>Following</li>
+            </ul>
+        </section>
+    )
+}
