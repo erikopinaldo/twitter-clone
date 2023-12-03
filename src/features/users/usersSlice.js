80f80cd4764883e00c017f078b21c63d20e550c1
Erik Opinaldo <erikopinaldo@gmail.com>
2023-12-03 16:38:44 -0500

    add following array to initial user state

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,9 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = [
-    { id: '0', name: 'Tianna Jenkins', username: "@tjenkins" },
-    { id: '1', name: 'Kevin Grant', username: "@kgrant" },
-    { id: '2', name: 'Madison Price', username: "@mprice" }
+    { id: '0', name: 'Tianna Jenkins', username: "@tjenkins", following: [2] },
+    { id: '1', name: 'Kevin Grant', username: "@kgrant", following: [] },
+    { id: '2', name: 'Madison Price', username: "@mprice", following: [] }
 ]
 
 const usersSlice = createSlice({
