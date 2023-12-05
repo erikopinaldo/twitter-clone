f88a6ac601e8043496c493475bb28edd89edb3fa
Erik Opinaldo <erikopinaldo@gmail.com>
2023-12-04 19:43:17 -0500

    update default follow list for Tianna

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = [
-    { id: '0', name: 'Tianna Jenkins', username: "@tjenkins", following: [2] },
+    { id: '0', name: 'Tianna Jenkins', username: "@tjenkins", following: ['1'] },
     { id: '1', name: 'Kevin Grant', username: "@kgrant", following: [] },
     { id: '2', name: 'Madison Price', username: "@mprice", following: [] }
 ]
