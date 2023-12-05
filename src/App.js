3bfd7bf1536be5112a5973149bd4b4d2e9a7bf72
Erik Opinaldo <erikopinaldo@gmail.com>
2023-12-04 19:22:12 -0500

    change ViewSelector folder

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import { PostsList } from './features/posts/PostsList'
 import { AddPostForm } from './features/posts/AddPostForm'
 import { MobileAddPostForm } from './features/posts/MobileAddPostForm'
 import { UserDropdown } from './features/currentUser/UserDropdown'
-import { ViewSelector } from './features/posts/ViewSelector'
+import { ViewSelector } from './features/currentTimelineView/ViewSelector'
 
 function App() {
   return (
