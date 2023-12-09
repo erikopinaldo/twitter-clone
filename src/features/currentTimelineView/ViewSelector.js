c57425e32ee214f058b2060b36ee2596e9f697d9
Erik Opinaldo <erikopinaldo@gmail.com>
2023-12-09 10:57:19 -0500

    convert ul to div

--- src/features/currentTimelineView/ViewSelector.js
+++ src/features/currentTimelineView/ViewSelector.js
@@ -4,10 +4,10 @@ import { ViewSelectorItem } from './ViewSelectorItem'
 export const ViewSelector = () => {
     return (
         <section className='view-selector-wrapper'>
-            <ul className='view-selector'>
+            <div className='view-selector'>
                 <ViewSelectorItem>For You</ViewSelectorItem>
                 <ViewSelectorItem>Following</ViewSelectorItem>
-            </ul>
+            </div>
         </section>
     )
 }
