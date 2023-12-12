0ba9fce2016a7e21558255381556c36dc43591c7
Erik Opinaldo <erikopinaldo@gmail.com>
2023-12-11 23:13:25 -0500

    Revert "add user dropdown menu reducer"
    This reverts commit d41b71e8ecc1f69bb27dfa55d07cd6ed24f10a4e.

--- src/app/store.js
+++ src/app/store.js
@@ -4,7 +4,6 @@ import postsReducer from '../features/posts/postsSlice'
 import usersReducer from '../features/users/usersSlice'
 import currentUserReducer from '../features/currentUser/currentUsersSlice'
 import currentTimelineViewReducer from '../features/currentTimelineView/currentTimelineViewSlice'
-import userDropdownMenuReducer from '../features/userDropdownMenu/userDropdownMenuSlice'
   
 export default configureStore({
   reducer: {
@@ -12,6 +11,5 @@ export default configureStore({
     users: usersReducer,
     currentUser: currentUserReducer,
     currentTimelineView: currentTimelineViewReducer,
-    userDropdownMenu: userDropdownMenuReducer,
   }
 })
