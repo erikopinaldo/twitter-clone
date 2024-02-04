63b0bc069ce67b2b86ff3773a6a61ad5e7c7f2f6
Erik Opinaldo <erikopinaldo@gmail.com>
2024-02-04 15:04:44 -0500

    add more dummy tweets

--- src/features/posts/initialTweetList.js
+++ src/features/posts/initialTweetList.js
@@ -41,6 +41,566 @@ const initialTweetList = [
             },
         }
     },
+    {
+        id: '2',
+        content: 'More text',
+        user: '2',
+        date: sub(new Date(), { minutes: 5 }).toISOString(),
+        reactions: {
+            heart: {
+                count: 0,
+                users: [],
+            },
+            retweet: {
+                count: 0,
+                users: [],
+            },
+            reply: {
+                count: 0,
+                users: [],
+            },
+        }
+    },
+    {
+        id: '1',
+        content: 'Hello!',
+        user: '0',
+        date: sub(new Date(), { minutes: 10 }).toISOString(),
+        reactions: {
+            heart: {
+                count: 0,
+                users: [],
+            },
+            retweet: {
+                count: 0,
+                users: [],
+            },
+            reply: {
+                count: 0,
+                users: [],
+            },
+        }
+    },
+    {
+        id: '2',
+        content: 'More text',
+        user: '2',
+        date: sub(new Date(), { minutes: 5 }).toISOString(),
+        reactions: {
+            heart: {
+                count: 0,
+                users: [],
+            },
+            retweet: {
+                count: 0,
+                users: [],
+            },
+            reply: {
+                count: 0,
+                users: [],
+            },
+        }
+    },
+    {
+        id: '2',
+        content: 'More text',
+        user: '2',
+        date: sub(new Date(), { minutes: 5 }).toISOString(),
+        reactions: {
+            heart: {
+                count: 0,
+                users: [],
+            },
+            retweet: {
+                count: 0,
+                users: [],
+            },
+            reply: {
+                count: 0,
+                users: [],
+            },
+        }
+    },
+    {
+        id: '1',
+        content: 'Hello!',
+        user: '0',
+        date: sub(new Date(), { minutes: 10 }).toISOString(),
+        reactions: {
+            heart: {
+                count: 0,
+                users: [],
+            },
+            retweet: {
+                count: 0,
+                users: [],
+            },
+            reply: {
+                count: 0,
+                users: [],
+            },
+        }
+    },
+    {
+        id: '2',
+        content: 'More text',
+        user: '2',
+        date: sub(new Date(), { minutes: 5 }).toISOString(),
+        reactions: {
+            heart: {
+                count: 0,
+                users: [],
+            },
+            retweet: {
+                count: 0,
+                users: [],
+            },
+            reply: {
+                count: 0,
+                users: [],
+            },
+        }
+    },
+    {
+        id: '2',
+        content: 'More text',
+        user: '2',
+        date: sub(new Date(), { minutes: 5 }).toISOString(),
+        reactions: {
+            heart: {
+                count: 0,
+                users: [],
+            },
+            retweet: {
+                count: 0,
+                users: [],
+            },
+            reply: {
+                count: 0,
+                users: [],
+            },
+        }
+    },
+    {
+        id: '1',
+        content: 'Hello!',
+        user: '0',
+        date: sub(new Date(), { minutes: 10 }).toISOString(),
+        reactions: {
+            heart: {
+                count: 0,
+                users: [],
+            },
+            retweet: {
+                count: 0,
+                users: [],
+            },
+            reply: {
+                count: 0,
+                users: [],
+            },
+        }
+    },
+    {
+        id: '2',
+        content: 'More text',
+        user: '2',
+        date: sub(new Date(), { minutes: 5 }).toISOString(),
+        reactions: {
+            heart: {
+                count: 0,
+                users: [],
+            },
+            retweet: {
+                count: 0,
+                users: [],
+            },
+            reply: {
+                count: 0,
+                users: [],
+            },
+        }
+    },
+    {
+        id: '2',
+        content: 'More text',
+        user: '2',
+        date: sub(new Date(), { minutes: 5 }).toISOString(),
+        reactions: {
+            heart: {
+                count: 0,
+                users: [],
+            },
+            retweet: {
+                count: 0,
+                users: [],
+            },
+            reply: {
+                count: 0,
+                users: [],
+            },
+        }
+    },
+    {
+        id: '1',
+        content: 'Hello!',
+        user: '0',
+        date: sub(new Date(), { minutes: 10 }).toISOString(),
+        reactions: {
+            heart: {
+                count: 0,
+                users: [],
+            },
+            retweet: {
+                count: 0,
+                users: [],
+            },
+            reply: {
+                count: 0,
+                users: [],
+            },
+        }
+    },
+    {
+        id: '2',
+        content: 'More text',
+        user: '2',
+        date: sub(new Date(), { minutes: 5 }).toISOString(),
+        reactions: {
+            heart: {
+                count: 0,
+                users: [],
+            },
+            retweet: {
+                count: 0,
+                users: [],
+            },
+            reply: {
+                count: 0,
+                users: [],
+            },
+        }
+    },
+    {
+        id: '2',
+        content: 'More text',
+        user: '2',
+        date: sub(new Date(), { minutes: 5 }).toISOString(),
+        reactions: {
+            heart: {
+                count: 0,
+                users: [],
+            },
+            retweet: {
+                count: 0,
+                users: [],
+            },
+            reply: {
+                count: 0,
+                users: [],
+            },
+        }
+    },
+    {
+        id: '1',
+        content: 'Hello!',
+        user: '0',
+        date: sub(new Date(), { minutes: 10 }).toISOString(),
+        reactions: {
+            heart: {
+                count: 0,
+                users: [],
+            },
+            retweet: {
+                count: 0,
+                users: [],
+            },
+            reply: {
+                count: 0,
+                users: [],
+            },
+        }
+    },
+    {
+        id: '2',
+        content: 'More text',
+        user: '2',
+        date: sub(new Date(), { minutes: 5 }).toISOString(),
+        reactions: {
+            heart: {
+                count: 0,
+                users: [],
+            },
+            retweet: {
+                count: 0,
+                users: [],
+            },
+            reply: {
+                count: 0,
+                users: [],
+            },
+        }
+    },
+    {
+        id: '2',
+        content: 'More text',
+        user: '2',
+        date: sub(new Date(), { minutes: 5 }).toISOString(),
+        reactions: {
+            heart: {
+                count: 0,
+                users: [],
+            },
+            retweet: {
+                count: 0,
+                users: [],
+            },
+            reply: {
+                count: 0,
+                users: [],
+            },
+        }
+    },
+    {
+        id: '1',
+        content: 'Hello!',
+        user: '0',
+        date: sub(new Date(), { minutes: 10 }).toISOString(),
+        reactions: {
+            heart: {
+                count: 0,
+                users: [],
+            },
+            retweet: {
+                count: 0,
+                users: [],
+            },
+            reply: {
+                count: 0,
+                users: [],
+            },
+        }
+    },
+    {
+        id: '2',
+        content: 'More text',
+        user: '2',
+        date: sub(new Date(), { minutes: 5 }).toISOString(),
+        reactions: {
+            heart: {
+                count: 0,
+                users: [],
+            },
+            retweet: {
+                count: 0,
+                users: [],
+            },
+            reply: {
+                count: 0,
+                users: [],
+            },
+        }
+    },
+    {
+        id: '2',
+        content: 'More text',
+        user: '2',
+        date: sub(new Date(), { minutes: 5 }).toISOString(),
+        reactions: {
+            heart: {
+                count: 0,
+                users: [],
+            },
+            retweet: {
+                count: 0,
+                users: [],
+            },
+            reply: {
+                count: 0,
+                users: [],
+            },
+        }
+    },
+    {
+        id: '1',
+        content: 'Hello!',
+        user: '0',
+        date: sub(new Date(), { minutes: 10 }).toISOString(),
+        reactions: {
+            heart: {
+                count: 0,
+                users: [],
+            },
+            retweet: {
+                count: 0,
+                users: [],
+            },
+            reply: {
+                count: 0,
+                users: [],
+            },
+        }
+    },
+    {
+        id: '2',
+        content: 'More text',
+        user: '2',
+        date: sub(new Date(), { minutes: 5 }).toISOString(),
+        reactions: {
+            heart: {
+                count: 0,
+                users: [],
+            },
+            retweet: {
+                count: 0,
+                users: [],
+            },
+            reply: {
+                count: 0,
+                users: [],
+            },
+        }
+    },
+    {
+        id: '2',
+        content: 'More text',
+        user: '2',
+        date: sub(new Date(), { minutes: 5 }).toISOString(),
+        reactions: {
+            heart: {
+                count: 0,
+                users: [],
+            },
+            retweet: {
+                count: 0,
+                users: [],
+            },
+            reply: {
+                count: 0,
+                users: [],
+            },
+        }
+    },
+    {
+        id: '1',
+        content: 'Hello!',
+        user: '0',
+        date: sub(new Date(), { minutes: 10 }).toISOString(),
+        reactions: {
+            heart: {
+                count: 0,
+                users: [],
+            },
+            retweet: {
+                count: 0,
+                users: [],
+            },
+            reply: {
+                count: 0,
+                users: [],
+            },
+        }
+    },
+    {
+        id: '2',
+        content: 'More text',
+        user: '2',
+        date: sub(new Date(), { minutes: 5 }).toISOString(),
+        reactions: {
+            heart: {
+                count: 0,
+                users: [],
+            },
+            retweet: {
+                count: 0,
+                users: [],
+            },
+            reply: {
+                count: 0,
+                users: [],
+            },
+        }
+    },
+    {
+        id: '2',
+        content: 'More text',
+        user: '2',
+        date: sub(new Date(), { minutes: 5 }).toISOString(),
+        reactions: {
+            heart: {
+                count: 0,
+                users: [],
+            },
+            retweet: {
+                count: 0,
+                users: [],
+            },
+            reply: {
+                count: 0,
+                users: [],
+            },
+        }
+    },
+    {
+        id: '1',
+        content: 'Hello!',
+        user: '0',
+        date: sub(new Date(), { minutes: 10 }).toISOString(),
+        reactions: {
+            heart: {
+                count: 0,
+                users: [],
+            },
+            retweet: {
+                count: 0,
+                users: [],
+            },
+            reply: {
+                count: 0,
+                users: [],
+            },
+        }
+    },
+    {
+        id: '2',
+        content: 'More text',
+        user: '2',
+        date: sub(new Date(), { minutes: 5 }).toISOString(),
+        reactions: {
+            heart: {
+                count: 0,
+                users: [],
+            },
+            retweet: {
+                count: 0,
+                users: [],
+            },
+            reply: {
+                count: 0,
+                users: [],
+            },
+        }
+    },
+    {
+        id: '2',
+        content: 'More text',
+        user: '2',
+        date: sub(new Date(), { minutes: 5 }).toISOString(),
+        reactions: {
+            heart: {
+                count: 0,
+                users: [],
+            },
+            retweet: {
+                count: 0,
+                users: [],
+            },
+            reply: {
+                count: 0,
+                users: [],
+            },
+        }
+    },
 ]
 
 export default initialTweetList
