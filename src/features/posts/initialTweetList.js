4229e37dd89b350193b8b7ffbd0e08c098a68a63
Erik Opinaldo <erikopinaldo@gmail.com>
2024-02-04 15:02:57 -0500

    move initial tweet list to separate file

---
+++ src/features/posts/initialTweetList.js
@@ -0,0 +1,46 @@
+import { sub } from 'date-fns'
+
+const initialTweetList = [
+    {
+        id: '1',
+        content: 'Hello!',
+        user: '0',
+        date: sub(new Date(), { minutes: 10 }).toISOString(),
+        reactions: {
+            heart: {
+                count: 0,
+                users: [],
+            },
+            retweet: {
+                count: 0,
+                users: [],
+            },
+            reply: {
+                count: 0,
+                users: [],
+            },
+        }
+    },
+    {
+        id: '2',
+        content: 'More text',
+        user: '2',
+        date: sub(new Date(), { minutes: 5 }).toISOString(),
+        reactions: {
+            heart: {
+                count: 0,
+                users: [],
+            },
+            retweet: {
+                count: 0,
+                users: [],
+            },
+            reply: {
+                count: 0,
+                users: [],
+            },
+        }
+    },
+]
+
+export default initialTweetList
