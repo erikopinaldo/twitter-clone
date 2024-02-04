5059e96959abe2d72c38a4ca95a91fb33786121f
Erik Opinaldo <erikopinaldo@gmail.com>
2024-02-04 16:09:48 -0500

    add profile pic bubble

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
+import { ProfilePicBubble } from '../users/ProfilePicBubble'
+
 import { postAdded } from './postsSlice'
 
 export const AddPostForm = ({ handleCloseClick }) => {
@@ -34,21 +36,24 @@ export const AddPostForm = ({ handleCloseClick }) => {
 
     return (
         <section className='add-post-form'>
-            <form onSubmit={onSavePostClicked}>
-                <textarea
-                    id="postContent"
-                    name="postContent"
-                    placeholder='What is happening?!'
-                    value={content}
-                    onChange={onContentChanged}
-                    onKeyDown={handleUserKeyPress}
-                />
-                <div className='tweet-button-container'>
-                    <button type="button" onClick={onSavePostClicked} disabled={!canSave}>
-                        Post
-                    </button>
-                </div>
-            </form>
+            <ProfilePicBubble styleOptions='timeline-profile-pic-bubble'/>
+            <div className='add-post-form-content-container'>
+                <form onSubmit={onSavePostClicked}>
+                    <textarea
+                        id="postContent"
+                        name="postContent"
+                        placeholder='What is happening?!'
+                        value={content}
+                        onChange={onContentChanged}
+                        onKeyDown={handleUserKeyPress}
+                    />
+                    <div className='tweet-button-container'>
+                        <button type="button" onClick={onSavePostClicked} disabled={!canSave}>
+                            Post
+                        </button>
+                    </div>
+                </form>
+            </div>     
         </section>
     )
 }
