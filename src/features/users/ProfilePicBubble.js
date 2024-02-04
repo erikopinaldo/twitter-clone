78caaffbcbe5833ef0a21a8eb888f1b02a3411e2
Erik Opinaldo <erikopinaldo@gmail.com>
2024-02-04 15:31:41 -0500

    create abstract profile pic bubble into separate component

---
+++ src/features/users/ProfilePicBubble.js
@@ -0,0 +1,10 @@
+export const ProfilePicBubble = () => {
+    return (
+        <div className='profile-pic-container'>
+            {/* <img
+                            className='profile-pic'
+                            src='https://pbs.twimg.com/profile_images/1506072421002104842/9GVCifCE_normal.jpg' /> */}
+            <div className='profile-pic'></div>
+        </div>
+    )
+}
