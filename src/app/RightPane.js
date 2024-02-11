e90efc2e4cc33f3d595b01fd253799a4e030b5ab
Erik Opinaldo <erikopinaldo@gmail.com>
2024-02-11 15:41:45 -0500

    initial whats happening card

--- src/app/RightPane.js
+++ src/app/RightPane.js
@@ -11,7 +11,7 @@ export const RightPane = () => {
                 </div>
                 <div className='trending-container'>
                     <div className='trending-container-spacer'></div>
-                    <div className='subscribe-card-container'>
+                    <div className='subscribe-card-container trending-container-item'>
                         <aside className='subscribe-card'>
                             <div className='subscribe-text-container'>
                                 <span className='subscribe-text'>Subscribe to Premium</span>
@@ -24,7 +24,111 @@ export const RightPane = () => {
                             </div>
                         </aside>
                     </div>
-                    <div className='trending-card-container'></div>
+                    <div className='trending-card-container'>
+                        <div className='whats-happening-container trending-card-item'>
+                            <h2>What's happening</h2>
+                        </div>
+                        <div className='trending-card-item'>
+                            <div className='trending-card-item-headline'>
+                                <span>Super Bowl LVIII: 49ers at Chiefs</span>
+                            </div>
+                            <div className='trending-card-item-subheading-container'>
+                                <div className='trending-card-item-subheading-item'>
+                                    <span>Super Bowl LVIII</span>
+                                </div>
+                                <div className='trending-card-item-subheading-item subheading-spacer'>
+                                    <span>·</span>
+                                </div>
+                                <div className='trending-card-item-subheading-item'>
+                                    <span>Starting at 5:30PM</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='trending-card-item'>
+                            <div className='trending-card-item-subheading-container'>
+                                <div className='trending-card-item-subheading-item'>
+                                    <span>Sports</span>
+                                </div>
+                                <div className='trending-card-item-subheading-item subheading-spacer'>
+                                    <span>·</span>
+                                </div>
+                                <div className='trending-card-item-subheading-item'>
+                                    <span>Trending</span>
+                                </div>
+                            </div>
+                            <div className='trending-card-item-headline'>
+                                <span>#AFCONFinal</span>
+                            </div>
+                            <div className='trending-card-item-subheading-container'>
+                                <div className='trending-card-item-subheading-item'>
+                                    <span>95.2K posts</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='trending-card-item'>
+                            <div className='trending-card-item-subheading-container'>
+                                <div className='trending-card-item-subheading-item'>
+                                    <span>Music</span>
+                                </div>
+                                <div className='trending-card-item-subheading-item subheading-spacer'>
+                                    <span>·</span>
+                                </div>
+                                <div className='trending-card-item-subheading-item'>
+                                    <span>Trending</span>
+                                </div>
+                            </div>
+                            <div className='trending-card-item-headline'>
+                                <span>Justin Bieber</span>
+                            </div>
+                            <div className='trending-card-item-subheading-container'>
+                                <div className='trending-card-item-subheading-item'>
+                                    <span>95.1K posts</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='trending-card-item'>
+                            <div className='trending-card-item-subheading-container'>
+                                <div className='trending-card-item-subheading-item'>
+                                    <span>Technology</span>
+                                </div>
+                                <div className='trending-card-item-subheading-item subheading-spacer'>
+                                    <span>·</span>
+                                </div>
+                                <div className='trending-card-item-subheading-item'>
+                                    <span>Trending</span>
+                                </div>
+                            </div>
+                            <div className='trending-card-item-headline'>
+                                <span>macOS</span>
+                            </div>
+                            <div className='trending-card-item-subheading-container'>
+                                <div className='trending-card-item-subheading-item'>
+                                    <span>1,426 posts</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='trending-card-item'>
+                            <div className='trending-card-item-subheading-container'>
+                                <div className='trending-card-item-subheading-item'>
+                                    <span>Technology</span>
+                                </div>
+                                <div className='trending-card-item-subheading-item subheading-spacer'>
+                                    <span>·</span>
+                                </div>
+                                <div className='trending-card-item-subheading-item'>
+                                    <span>Trending</span>
+                                </div>
+                            </div>
+                            <div className='trending-card-item-headline'>
+                                <span>#webdeveloper</span>
+                            </div>
+                            <div className='trending-card-item-subheading-container'>
+                                <div className='trending-card-item-subheading-item'>
+                                    <span>2,385 posts</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <div className='who-to-follow-card-container'></div>
                 </div>
             </div>
